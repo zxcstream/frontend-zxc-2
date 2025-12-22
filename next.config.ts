0f0c7a6e5f4ec216d01201b0ccc58c5056a1af75
iamDaedalus <jerometecson.basc@gmail.com>
2025-12-22 18:52:03 +0800

    aaa

--- next.config.ts
+++ next.config.ts
@@ -17,71 +17,71 @@ const nextConfig: NextConfig = {
     qualities: [75, 100],
   },
 
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/(.*)",
-  //       headers: [
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value: `
-  //           default-src 'self';
-  //          script-src 'self' 'unsafe-inline' https://www.youtube.com;
-  //           connect-src 'self' https://api.themoviedb.org;
-  //           img-src 'self' https://image.tmdb.org data:;
-  //           style-src 'self' 'unsafe-inline';
-  //        frame-src 'self'
-  // https://zxcstream.xyz
-  // https://www.zxcstream.xyz
-  // https://vidsrc-embed.ru
-  // https://vidsrc.cc
-  // https://player.videasy.net
-  // https://111movies.com
-  // https://vidfast.pro
-  // https://vidnest.fun
-  // https://www.youtube.com;
-  //         `
-  //             .replace(/\s{2,}/g, " ")
-  //             .trim(),
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       source: "/sw.js",
-  //       headers: [
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value: `
-  //           default-src 'self';
-  //            script-src 'self' 'unsafe-inline' https://www.youtube.com;
-  //       connect-src 'self' https://api.themoviedb.org https://image.tmdb.org https://www.youtube.com;
-  //           img-src 'self' https://image.tmdb.org data:;
-  //         frame-src 'self'
-  // https://zxcstream.xyz
-  // https://www.zxcstream.xyz
-  // https://vidsrc-embed.ru
-  // https://vidsrc.cc
-  // https://player.videasy.net
-  // https://111movies.com
-  // https://vidfast.pro
-  // https://vidnest.fun
-  // https://www.youtube.com;
-  //         `
-  //             .replace(/\s{2,}/g, " ")
-  //             .trim(),
-  //         },
-  //         {
-  //           key: "Cache-Control",
-  //           value: "no-cache, no-store, must-revalidate",
-  //         },
-  //         {
-  //           key: "Content-Type",
-  //           value: "application/javascript; charset=utf-8",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+            default-src 'self';
+           script-src 'self' 'unsafe-inline' https://www.youtube.com;
+            connect-src 'self' https://api.themoviedb.org;
+            img-src 'self' https://image.tmdb.org data:;
+            style-src 'self' 'unsafe-inline';
+         frame-src 'self'
+  https://zxcstream.xyz
+  https://www.zxcstream.xyz
+  https://vidsrc-embed.ru
+  https://vidsrc.cc
+  https://player.videasy.net
+  https://111movies.com
+  https://vidfast.pro
+  https://vidnest.fun
+  https://www.youtube.com;
+          `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+            default-src 'self';
+             script-src 'self' 'unsafe-inline' https://www.youtube.com;
+        connect-src 'self' https://api.themoviedb.org https://image.tmdb.org https://www.youtube.com;
+            img-src 'self' https://image.tmdb.org data:;
+          frame-src 'self'
+  https://zxcstream.xyz
+  https://www.zxcstream.xyz
+  https://vidsrc-embed.ru
+  https://vidsrc.cc
+  https://player.videasy.net
+  https://111movies.com
+  https://vidfast.pro
+  https://vidnest.fun
+  https://www.youtube.com;
+          `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Content-Type",
+            value: "application/javascript; charset=utf-8",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
